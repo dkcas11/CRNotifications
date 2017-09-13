59cc7a4739de555fd50aa8e59c253363cccbd6a2
Herrick Wolber <wolbereric@yahoo.fr>
2017-09-13 22:40:57 +0200

    Reindent files to be nice and clean

--- Pods/CRNotifications/CRNotifications/Classes/CRNotifications.swift
+++ Pods/CRNotifications/CRNotifications/Classes/CRNotifications.swift
@@ -11,47 +11,47 @@ import UIKit
 public class CRNotifications {
     
     public init(){}
-	
-	/// Enum for types of notifications
-	public enum CRNotificationType {
-		case success
-		case error
-		case info
-		
-		var color: UIColor {
-			switch self {
-			case .success: return UIColor.flatGreen
-			case .error: return UIColor.flatRed
-			case .info: return UIColor.flatGray
-			}
-		}
-		
-		var image: UIImage {
+    
+    /// Enum for types of notifications
+    public enum CRNotificationType {
+        case success
+        case error
+        case info
+        
+        var color: UIColor {
+            switch self {
+            case .success: return UIColor.flatGreen
+            case .error: return UIColor.flatRed
+            case .info: return UIColor.flatGray
+            }
+        }
+        
+        var image: UIImage {
             let bundle = Bundle(for: CRNotifications.self)
             
-			switch self {
-			case .success: return UIImage(named: "success", in: bundle, compatibleWith: nil)!
-			case .error: return UIImage(named: "error", in: bundle, compatibleWith: nil)!
-			case .info: return UIImage(named: "info", in: bundle, compatibleWith: nil)!
-			}
-		}
-	}
-
-	/// Shows a CRNotification
-	public static func showNotification(type: CRNotificationType, title: String, message: String, dismissDelay: TimeInterval) {
-		let view = CRNotification()
-
-		view.setBackgroundColor(color: type.color)
-		view.setImage(image: type.image)
-		view.setTitle(title: title)
-		view.setMessage(message: message)
-		view.setDismisTimer(delay: dismissDelay)
-
-		guard let window = UIApplication.shared.keyWindow else {
-			print("Failed to show CRNotification. No keywindow available.")
-			return
-		}
-		window.addSubview(view)
-		view.showNotification()
-	}
+            switch self {
+            case .success: return UIImage(named: "success", in: bundle, compatibleWith: nil)!
+            case .error: return UIImage(named: "error", in: bundle, compatibleWith: nil)!
+            case .info: return UIImage(named: "info", in: bundle, compatibleWith: nil)!
+            }
+        }
+    }
+    
+    /// Shows a CRNotification
+    public static func showNotification(type: CRNotificationType, title: String, message: String, dismissDelay: TimeInterval) {
+        let view = CRNotification()
+        
+        view.setBackgroundColor(color: type.color)
+        view.setImage(image: type.image)
+        view.setTitle(title: title)
+        view.setMessage(message: message)
+        view.setDismisTimer(delay: dismissDelay)
+        
+        guard let window = UIApplication.shared.keyWindow else {
+            print("Failed to show CRNotification. No keywindow available.")
+            return
+        }
+        window.addSubview(view)
+        view.showNotification()
+    }
 }
