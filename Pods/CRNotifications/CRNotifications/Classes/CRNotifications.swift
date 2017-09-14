eb4cab2e97edad36188eccdae97c68283b51632a
Casper Riboe <casper2602@hotmail.com>
2017-09-14 08:26:17 +0200

    Moved CRNotificationType to separate file to avoid being a class enum

--- Pods/CRNotifications/CRNotifications/Classes/CRNotifications.swift
+++ Pods/CRNotifications/CRNotifications/Classes/CRNotifications.swift
@@ -12,31 +12,6 @@ public class CRNotifications {
     
     public init(){}
     
-    /// Enum for types of notifications
-    public enum CRNotificationType {
-        case success
-        case error
-        case info
-        
-        var color: UIColor {
-            switch self {
-            case .success: return UIColor.flatGreen
-            case .error: return UIColor.flatRed
-            case .info: return UIColor.flatGray
-            }
-        }
-        
-        var image: UIImage {
-            let bundle = Bundle(for: CRNotifications.self)
-            
-            switch self {
-            case .success: return UIImage(named: "success", in: bundle, compatibleWith: nil)!
-            case .error: return UIImage(named: "error", in: bundle, compatibleWith: nil)!
-            case .info: return UIImage(named: "info", in: bundle, compatibleWith: nil)!
-            }
-        }
-    }
-    
     /// Shows a CRNotification
     public static func showNotification(type: CRNotificationType, title: String, message: String, dismissDelay: TimeInterval) {
         let view = CRNotification()
