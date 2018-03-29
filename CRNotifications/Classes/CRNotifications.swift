68c27995fd8057bcab65d19c0a9ffd7748ad7495
thoffmann <toni.hoffmann@arthrex.de>
2018-03-29 10:49:07 +0200

    option to pass custom background color

--- CRNotifications/Classes/CRNotifications.swift
+++ CRNotifications/Classes/CRNotifications.swift
@@ -12,16 +12,20 @@ public class CRNotifications {
     
     public init(){}
     
-    /// Shows a CRNotification
-	public static func showNotification(type: CRNotificationType, title: String, message: String, dismissDelay: TimeInterval, completion: @escaping () -> () = {}) {
+    /** Shows a CRNotification with a type, title, message, delay and completion callback */
+    public static func showNotification(type: CRNotificationType, title: String, message: String, dismissDelay: TimeInterval, completion: @escaping () -> () = {}) {
+        showNotification(type: type, title: title, message: message, dismissDelay: dismissDelay, backgroundColor: type.color, completion: completion)
+    }
+    /** Shows a CRNotification with a type, title, message, delay, background color and completion callback */
+    public static func showNotification(type: CRNotificationType, title: String, message: String, dismissDelay: TimeInterval, backgroundColor: UIColor, completion: @escaping () -> () = {}) {
         let view = CRNotification()
         
-        view.setBackgroundColor(color: type.color)
+        view.setBackgroundColor(color: backgroundColor)
         view.setImage(image: type.image)
         view.setTitle(title: title)
         view.setMessage(message: message)
         view.setDismisTimer(delay: dismissDelay)
-		view.setCompletionBlock(completion)
+        view.setCompletionBlock(completion)
         
         guard let window = UIApplication.shared.keyWindow else {
             print("Failed to show CRNotification. No keywindow available.")
