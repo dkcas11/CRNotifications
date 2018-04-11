350efc9bf5bcc9be8d40aea73fcfdd767755eb01
Erfan Anwar <iamirfan@live.com>
2018-04-11 13:26:09 +0500

    Added functionality to show only 1 notification at a time and hide notification option

--- Pods/CRNotifications/CRNotifications/Classes/CRNotifications.swift
+++ Pods/CRNotifications/CRNotifications/Classes/CRNotifications.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 public class CRNotifications {
-    
+    static var notifications = [CRNotification]()
     public init(){}
     
     /// Shows a CRNotification
-	public static func showNotification(type: CRNotificationType, title: String, message: String, dismissDelay: TimeInterval, completion: @escaping () -> () = {}) {
+    public static func showNotification(type: CRNotificationType, title: String, message: String, dismissDelay: TimeInterval, completion: @escaping () -> () = {}) {
         let view = CRNotification()
         
         view.setBackgroundColor(color: type.color)
@@ -21,13 +21,32 @@ public class CRNotifications {
         view.setTitle(title: title)
         view.setMessage(message: message)
         view.setDismisTimer(delay: dismissDelay)
-		view.setCompletionBlock(completion)
+        view.setCompletionBlock(completion)
         
         guard let window = UIApplication.shared.keyWindow else {
             print("Failed to show CRNotification. No keywindow available.")
             return
         }
+        
+        notifications.removeAll()
+        notifications.append(view)
         window.addSubview(view)
         view.showNotification()
+    }
+    
+    public static func isNotificationVisible() -> Bool {
+        if CRNotifications.notifications.count > 0 && CRNotifications.notifications[0].superview != nil {
+            return true
+        } else {
+            return false
+        }
+        
+    }
+    
+    public static func hideNotification() {
+        for notification in CRNotifications.notifications {
+            notification.dismissNotification()
+            notifications.removeAll()
+        }
     }
 }
